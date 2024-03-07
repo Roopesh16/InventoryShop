dfbf996dec142b2b66f88d5bed9f7c156fbbc41e
Roopesh Verma <roopeshv.30@gmail.com>
2024-03-07 22:18:32 +0530

    Add Item Scriptable Object script

---
+++ Assets/Scripts/Items/ItemScriptableObject.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+namespace InventoryShop.Items
+{
+    [CreateAssetMenu(fileName = "ItemScriptableObject", menuName = "Items/ItemScriptableObject")]
+    public class ItemScriptableObject : ScriptableObject
+    {
+        public ItemType itemType;
+        public Image itemIcon;
+        public string itemDescription;
+        public int itemBuyPrice;
+        public int itemSellPrice;
+        public float itemWeight;
+        public ItemRarity itemRarity;
+        public int itemQuantity;
+    }
+
+    [CreateAssetMenu(fileName="ItemSOList", menuName="Items/ItemSOList")]
+    public class ItemSOList:ScriptableObject
+    {
+        public List<ItemScriptableObject> itemList = new();
+    }
+}
