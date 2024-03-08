4587c8d70cdf2c51b86c573bba865d724c08cb6e
Roopesh Verma <roopeshv.30@gmail.com>
2024-03-08 21:00:26 +0530

    Add Buy Box Model

---
+++ Assets/Scripts/Shop/Buy/BuyBoxModel.cs
@@ -0,0 +1,24 @@
+public class BuyBoxModel
+{
+    #region --------- Private Variables ---------
+    private BuyBoxController buyBoxController;
+    #endregion ------------------
+
+    #region --------- Public Variables ---------
+    public int itemCount;
+    public int itemBuyCost;
+    #endregion ------------------
+
+    #region --------- Private Methods ---------
+    #endregion ------------------
+
+    #region --------- Public Methods ---------
+    public BuyBoxModel(int itemCount,int itemBuyCost)
+    {
+        this.itemCount = itemCount;
+        this.itemBuyCost = itemBuyCost;
+    }
+
+    public SetBuyBoxController(BuyBoxController buyBoxController) => this.buyBoxController = buyBoxController;
+    #endregion ------------------
+}
