141c8aee44e4b2b67869f8733cbb872cf7c2301e
Roopesh Verma <roopeshv.30@gmail.com>
2024-03-08 22:19:14 +0530

    Add extra parameter in event

--- Assets/Scripts/Events/EventController.cs
+++ Assets/Scripts/Events/EventController.cs
@@ -1,17 +1,18 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace InventoryShop.Events
 {
-    public class EventController<T, U, V, W>
+    public class EventController<T, U, V, W,X>
     {
         #region --------- Public Variables ---------
-        public event Action<T, U, V, W> baseEvent;
+        public event Action<T, U, V, W, X> baseEvent;
         #endregion ------------------
 
         #region --------- Public Methods ---------
-        public void InvokeEvent(T var1, U var2, V var3, W var4) => baseEvent?.Invoke(var1, var2, var3, var4);
-        public void AddListener(Action<T, U, V, W> listener) => baseEvent += listener;
-        public void RemoveListener(Action<T, U, V, W> listener) => baseEvent -= listener;
+        public void InvokeEvent(T var1, U var2, V var3, W var4, X var5) => baseEvent?.Invoke(var1, var2, var3, var4, var5);
+        public void AddListener(Action<T, U, V, W,X> listener) => baseEvent += listener;
+        public void RemoveListener(Action<T, U, V, W,X> listener) => baseEvent -= listener;
         #endregion ------------------
     }
 }
