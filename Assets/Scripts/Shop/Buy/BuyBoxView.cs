4a67a8ad879051c0908c26edc05c113d32835476
Roopesh Verma <roopeshv.30@gmail.com>
2024-03-08 22:19:53 +0530

    Apply count btn click interface

--- Assets/Scripts/Shop/Buy/BuyBoxView.cs
+++ Assets/Scripts/Shop/Buy/BuyBoxView.cs
@@ -1,12 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace InventoryShop.Shop.BuyBox
 {
-    public class BuyBoxView : MonoBehaviour
+    public class BuyBoxView : MonoBehaviour, ICountBtnClick
     {
         #region --------- Serialized Variables ---------
+        [SerializeField] private Button negativeBtn;
+        [SerializeField] private Button positiveBtn;
+        [SerializeField] private TextMeshProUGUI countText;
+        [SerializeField] private TextMeshProUGUI itemBuyText;
         #endregion ------------------
 
         #region --------- Private Variables ---------
@@ -20,12 +24,58 @@ namespace InventoryShop.Shop.BuyBox
         #endregion ------------------
 
         #region --------- Private Methods ---------
+
         #endregion ------------------
 
         #region --------- Public Methods ---------
-        public void SetBuyBoxView() => gameObject.SetActive(false);
-
+        public void SetBuyBoxView(int itemCount)
+        {
+            gameObject.SetActive(false);
+            countText.text = itemCount.ToString();
+        }
         public void SetBuyBoxController(BuyBoxController buyBoxController) => this.buyBoxController = buyBoxController;
+
+        public void OnNegativeBtnClick() => buyBoxController.IncrementItemCount();
+
+        public void OnPositiveBtnClick() => buyBoxController.DecrementItemCount();
+
+        public void UpdateBuyCounter(int itemCount, int itemBuyCost)
+        {
+            countText.text = itemCount.ToString();
+            itemBuyText.text = (itemCount * itemBuyCost).ToString();
+        }
+
+        public void EnableNegativeBtn()
+        {
+            if (negativeBtn.interactable)
+                return;
+
+            negativeBtn.interactable = true;
+        }
+
+        public void EnablePositiveBtn()
+        {
+            if (positiveBtn.interactable)
+                return;
+
+            positiveBtn.interactable = true;
+        }
+
+        public void DisablePositiveBtn()
+        {
+            if (!positiveBtn.interactable)
+                return;
+
+            positiveBtn.interactable = false;
+        }
+
+        public void DisableNegativeBtn()
+        {
+            if (!negativeBtn.interactable)
+                return;
+
+            negativeBtn.interactable = false;
+        }
         #endregion ------------------
     }
 }
