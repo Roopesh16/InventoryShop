da3c5b5bf94210aa7efc4c2d32a9b0fe65d1ab4d
Roopesh Verma <roopeshv.30@gmail.com>
2024-03-09 20:58:18 +0530

    Remove instance from event

--- Assets/Scripts/Events/EventService.cs
+++ Assets/Scripts/Events/EventService.cs
@@ -4,22 +4,7 @@ namespace InventoryShop.Events
 {
     public class EventService
     {
-        #region --------- Private Variables ---------
-        private static EventService instance = null;
-        #endregion ------------------
-
         #region --------- Public Variables ---------
-        public static EventService Instance
-        {
-            get
-            {
-                if (instance == null)
-                    instance = new EventService();
-
-                return instance;
-            }
-        }
-
         public EventController<string, Sprite, string, int, int> OnItemClick;
         #endregion ------------------
 
