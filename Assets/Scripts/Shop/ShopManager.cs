8626e5377d2b936e7fc868b30fe19ca524b0babb
Roopesh Verma <roopeshv.30@gmail.com>
2024-03-09 15:39:09 +0530

    Disable type grids

--- Assets/Scripts/Shop/ShopManager.cs
+++ Assets/Scripts/Shop/ShopManager.cs
@@ -43,14 +43,14 @@ namespace InventoryShop.Managers
         private void Start()
         {
             ItemManager.Instance.SpawnItems(shopGridTransform);
-            ItemManager.Instance.SpawnItems(materialGridTransform, ItemType.MATERIAL);
-            materialGridTransform.gameObject.SetActive(false);
-            ItemManager.Instance.SpawnItems(weaponGridTransform, ItemType.WEAPON);
-            weaponGridTransform.gameObject.SetActive(false);
-            ItemManager.Instance.SpawnItems(consumableGridTransform, ItemType.CONSUMABLE);
-            consumableGridTransform.gameObject.SetActive(false);
-            ItemManager.Instance.SpawnItems(treasureGridTransform, ItemType.TREASURE);
-            treasureGridTransform.gameObject.SetActive(false);
+            // ItemManager.Instance.SpawnItems(materialGridTransform, ItemType.MATERIAL);
+            // materialGridTransform.gameObject.SetActive(false);
+            // ItemManager.Instance.SpawnItems(weaponGridTransform, ItemType.WEAPON);
+            // weaponGridTransform.gameObject.SetActive(false);
+            // ItemManager.Instance.SpawnItems(consumableGridTransform, ItemType.CONSUMABLE);
+            // consumableGridTransform.gameObject.SetActive(false);
+            // ItemManager.Instance.SpawnItems(treasureGridTransform, ItemType.TREASURE);
+            // treasureGridTransform.gameObject.SetActive(false);
 
             ShopController shopController = new(shopView);
             buyBoxController = new(buyBoxView);
