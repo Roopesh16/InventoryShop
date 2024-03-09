5a8e8d6fedc81fde58143d4ddc884223746de0b7
Roopesh Verma <roopeshv.30@gmail.com>
2024-03-09 21:54:52 +0530

    Add Game Service

---
+++ Assets/Scripts/Main/GameService.cs
@@ -0,0 +1,63 @@
+using InventoryShop.ScriptableObjects;
+using System.Collections.Generic;
+using InventoryShop.Shop.BuyBox;
+using InventoryShop.Managers;
+using InventoryShop.Items;
+using UnityEngine;
+using InventoryShop.Events;
+
+namespace InventoryShop.Services
+{
+    public class GameService : MonoBehaviour
+    {
+        #region --------- Serialized Variables ---------
+        [Header("Item Service References")]
+        [SerializeField] private List<ItemScriptableObject> itemsList = new();
+        [SerializeField] private ItemView itemPrefab;
+
+        [Header("Player Service References")]
+        [SerializeField] private int currentMoney = 0;
+
+        [Header("Shop Service References")]
+        [SerializeField] private Transform shopGridTransform;
+        [SerializeField] private ShopView shopView;
+        [SerializeField] private BuyBoxView buyBoxView;
+        #endregion ------------------
+
+        #region --------- Private Variables ---------
+        private EventService eventService;
+        private ItemService itemService;
+        private PlayerService playerService;
+        private ShopService shopService;
+        #endregion ------------------
+
+        #region --------- Public Variables ---------
+        #endregion ------------------
+
+        #region --------- Monobehavior Methods ---------
+        private void Start()
+        {
+            CreateService();
+            InjectDependency();
+        }
+        #endregion ------------------
+
+        #region --------- Private Methods ---------
+        private void CreateService()
+        {
+            eventService = new();
+            playerService = new(currentMoney);
+            itemService = new(itemsList, itemPrefab);
+            shopService = new(shopGridTransform, shopView, buyBoxView);
+        }
+
+        private void InjectDependency()
+        {
+            shopService.Init(eventService, itemService);
+        }
+        #endregion ------------------
+
+        #region --------- Public Methods ---------
+        #endregion ------------------
+    }
+}
