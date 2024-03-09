9af30c3dd61c35a223b7363b1c713c7a0df8243a
Roopesh Verma <roopeshv.30@gmail.com>
2024-03-09 20:13:53 +0530

    remove grid reference

--- Assets/Scripts/Shop/ShopManager.cs
+++ Assets/Scripts/Shop/ShopManager.cs
@@ -7,12 +7,8 @@ namespace InventoryShop.Managers
     public class ShopManager : MonoBehaviour
     {
         #region --------- Serialized Variables ---------
-        [Header("Grid Transforms")]
+        [Header("Grid Transform")]
         [SerializeField] private Transform shopGridTransform;
-        [SerializeField] private Transform materialGridTransform;
-        [SerializeField] private Transform weaponGridTransform;
-        [SerializeField] private Transform consumableGridTransform;
-        [SerializeField] private Transform treasureGridTransform;
 
         [Header("Reference")]
         [SerializeField] private ShopView shopView;
@@ -50,14 +46,6 @@ namespace InventoryShop.Managers
         public void Init()
         {
             ItemManager.Instance.SpawnItems(shopGridTransform);
-            // ItemManager.Instance.SpawnItems(materialGridTransform, ItemType.MATERIAL);
-            // materialGridTransform.gameObject.SetActive(false);
-            // ItemManager.Instance.SpawnItems(weaponGridTransform, ItemType.WEAPON);
-            // weaponGridTransform.gameObject.SetActive(false);
-            // ItemManager.Instance.SpawnItems(consumableGridTransform, ItemType.CONSUMABLE);
-            // consumableGridTransform.gameObject.SetActive(false);
-            // ItemManager.Instance.SpawnItems(treasureGridTransform, ItemType.TREASURE);
-            // treasureGridTransform.gameObject.SetActive(false);
 
             shopController = new(shopView);
             buyBoxController = new(buyBoxView);
