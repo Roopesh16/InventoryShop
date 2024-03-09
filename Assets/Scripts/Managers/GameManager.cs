c2a7db92c24661a06db426d4e432a619d9ee8f07
Roopesh Verma <roopeshv.30@gmail.com>
2024-03-09 18:03:00 +0530

    Rename

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -39,13 +39,13 @@ namespace InventoryShop.Managers
         #region --------- Public Methods ---------
         public void ValidateBuyTransaction(int itemCount, int itemBuyCost)
         {
-            if (PlayerManager.Instance.GetCurrentCost() == 0)
+            if (PlayerManager.Instance.GetCurrentMoney() == 0)
             {
                 UIManager.Instance.SetNotificationText("NO MONEY!");
                 return;
             }
 
-            if (itemBuyCost > PlayerManager.Instance.GetCurrentCost())
+            if (itemBuyCost > PlayerManager.Instance.GetCurrentMoney())
             {
                 UIManager.Instance.SetNotificationText("EXCEED COST!");
                 return;
