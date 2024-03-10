ca9da38a815462194d31c698677d15b510d2b99f
Roopesh Verma <roopeshv.30@gmail.com>
2024-03-10 16:49:57 +0530

    Send item so

--- Assets/Scripts/Main/GameService.cs
+++ Assets/Scripts/Main/GameService.cs
@@ -60,7 +60,7 @@ namespace InventoryShop.Services
         {
             GameManager.Instance.Init(playerService, itemService);
             UIManager.Instance.Init(playerService);
-            shopService.Init(eventService, itemService);
+            shopService.Init(eventService, itemService, itemsList);
         }
         #endregion ------------------
 
