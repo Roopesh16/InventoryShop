ed31b729b610239aa1d0355c1a3f17b5f51291be
Roopesh Verma <roopeshv.30@gmail.com>
2024-03-10 18:55:23 +0530

    Add sell box scripts

---
+++ Assets/Scripts/Inventory/Sell/SellBoxController.cs
@@ -0,0 +1,86 @@
+using InventoryShop.Services;
+
+namespace InventoryShop.Shop.BuyBox
+{
+    public class SellBoxController
+    {
+    //     #region --------- Private Variables ---------
+    //     private ShopService shopService;
+
+    //     private BuyBoxModel buyBoxModel;
+    //     private BuyBoxView buyBoxView;
+
+    //     private const int itemCount = 0;
+    //     #endregion ------------------
+
+    //     #region --------- Public Variables ---------
+    //     #endregion ------------------
+
+    //     #region --------- Private Methods ---------
+    //     #endregion ------------------
+
+    //     #region --------- Public Methods ---------
+    //     public BuyBoxController(BuyBoxView buyBoxView, ShopService shopService)
+    //     {
+    //         this.shopService = shopService;
+
+    //         buyBoxModel = new(itemCount);
+    //         buyBoxModel.SetBuyBoxController(this);
+
+    //         this.buyBoxView = buyBoxView;
+    //         this.buyBoxView.SetBuyBoxView(itemCount);
+    //         this.buyBoxView.SetBuyBoxController(this);
+    //     }
+
+    //     public void IncrementItemCount()
+    //     {
+    //         buyBoxModel.itemCount++;
+    //         buyBoxView.EnableNegativeBtn();
+    //         if (buyBoxModel.itemCount >= buyBoxModel.itemQuantity)
+    //         {
+    //             buyBoxModel.itemCount = buyBoxModel.itemQuantity;
+    //             buyBoxView.DisablePositiveBtn();
+    //         }
+    //         buyBoxView.UpdateBuyCounter(buyBoxModel.itemCount, buyBoxModel.itemBuyCost);
+    //     }
+
+    //     public void DecrementItemCount()
+    //     {
+    //         buyBoxModel.itemCount--;
+    //         buyBoxView.EnablePositiveBtn();
+    //         if (buyBoxModel.itemCount <= 0)
+    //         {
+    //             buyBoxModel.itemCount = 0;
+    //             buyBoxView.DisableNegativeBtn();
+    //         }
+    //         buyBoxView.UpdateBuyCounter(buyBoxModel.itemCount, buyBoxModel.itemBuyCost);
+    //     }
+
+    //     public void SetBuyItemData(int itemBuyCost, int itemQuantity)
+    //     {
+    //         buyBoxModel.SetItemData(itemBuyCost, itemQuantity);
+    //         buyBoxView.EnableBuyBox();
+    //     }
+
+    //     public void ResetItemCounter(bool hasBought)
+    //     {
+    //         if (hasBought)
+    //         {
+    //             buyBoxModel.itemQuantity -= buyBoxModel.itemCount;
+    //             if (buyBoxModel.itemQuantity <= 0)
+    //             {
+    //                 buyBoxModel.itemQuantity = 0;
+    //                 shopService.DisableDescription();
+    //                 buyBoxView.DisablePositiveBtn();
+    //                 buyBoxView.DisableNegativeBtn();
+    //             }
+    //             shopService.SetItemQuantity(buyBoxModel.itemQuantity);
+    //         }
+
+    //         buyBoxModel.itemCount = 0;
+    //         buyBoxView.UpdateBuyCounter(buyBoxModel.itemCount, buyBoxModel.itemBuyCost);
+    //     }
+    //     #endregion ------------------
+    }
+}
+
---
+++ Assets/Scripts/Inventory/Sell/SellBoxView.cs
@@ -0,0 +1,88 @@
+using InventoryShop.Managers;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace InventoryShop.Shop.BuyBox
+{
+    // public class SellBoxView : MonoBehaviour, ICountBtnClick, IItemTransfer
+    // {
+        // #region --------- Serialized Variables ---------
+        // [SerializeField] private Button negativeBtn;
+        // [SerializeField] private Button positiveBtn;
+        // [SerializeField] private Button yesButton;
+        // [SerializeField] private Button noButton;
+        // [SerializeField] private TextMeshProUGUI countText;
+        // [SerializeField] private TextMeshProUGUI itemBuyText;
+        // #endregion ------------------
+
+        // #region --------- Private Variables ---------
+        // private BuyBoxController buyBoxController;
+        // private int itemCount;
+        // private int totalCost;
+        // #endregion ------------------
+
+        // #region --------- Public Variables ---------
+        // #endregion ------------------
+
+        // #region --------- Monobehavior Methods ---------
+        // private void Awake()
+        // {
+        //     negativeBtn.onClick.AddListener(OnNegativeBtnClick);
+        //     positiveBtn.onClick.AddListener(OnPositiveBtnClick);
+        //     yesButton.onClick.AddListener(OnYesClick);
+        //     noButton.onClick.AddListener(OnNoClick);
+        // }
+        // #endregion ------------------
+
+        // #region --------- Private Methods ---------
+
+        // #endregion ------------------
+
+        // #region --------- Public Methods ---------
+        // public void SetBuyBoxView(int itemCount)
+        // {
+        //     gameObject.SetActive(false);
+        //     countText.text = itemCount.ToString();
+        //     UpdateBuyCounter(itemCount, itemCount);
+        //     DisableNegativeBtn();
+        // }
+        // public void SetBuyBoxController(BuyBoxController buyBoxController) => this.buyBoxController = buyBoxController;
+
+        // public void OnNegativeBtnClick() => buyBoxController.DecrementItemCount();
+
+        // public void OnPositiveBtnClick() => buyBoxController.IncrementItemCount();
+
+        // public void UpdateBuyCounter(int itemCount, int itemBuyCost)
+        // {
+        //     this.itemCount = itemCount;
+        //     totalCost = itemCount * itemBuyCost;
+        //     countText.text = itemCount.ToString();
+        //     itemBuyText.text = totalCost.ToString();
+        // }
+
+        // public void OnYesClick()
+        // {
+        //     if(GameManager.Instance.ValidateBuyTransaction(itemCount, totalCost))
+        //         buyBoxController.ResetItemCounter(true);
+        // }
+
+        // public void OnNoClick()
+        // {
+        //     gameObject.SetActive(false);
+        //     buyBoxController.ResetItemCounter(false);
+        // }
+
+        // public void EnableNegativeBtn() => negativeBtn.interactable = true;
+
+        // public void EnablePositiveBtn() => positiveBtn.interactable = true;
+
+        // public void DisablePositiveBtn() => positiveBtn.interactable = false;
+
+        // public void DisableNegativeBtn() => negativeBtn.interactable = false;
+
+        // public void EnableBuyBox() => gameObject.SetActive(true);
+
+        // #endregion ------------------
+    // }
+}
