3fadfd090e02169f3562808b8d15a89d5d275b33
Roopesh Verma <roopeshv.30@gmail.com>
2024-03-11 12:48:18 +0530

    Add sell box view

--- Assets/Scripts/Inventory/Sell/SellBoxView.cs
+++ Assets/Scripts/Inventory/Sell/SellBoxView.cs
@@ -1,88 +1,88 @@
 using InventoryShop.Managers;
-using TMPro;
-using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
 
-namespace InventoryShop.Shop.BuyBox
+namespace InventoryShop.Inventory.SellBox
 {
-    // public class SellBoxView : MonoBehaviour, ICountBtnClick, IItemTransfer
-    // {
-        // #region --------- Serialized Variables ---------
-        // [SerializeField] private Button negativeBtn;
-        // [SerializeField] private Button positiveBtn;
-        // [SerializeField] private Button yesButton;
-        // [SerializeField] private Button noButton;
-        // [SerializeField] private TextMeshProUGUI countText;
-        // [SerializeField] private TextMeshProUGUI itemBuyText;
-        // #endregion ------------------
+    public class SellBoxView : MonoBehaviour, ICountBtnClick, IItemTransfer
+    {
+        #region --------- Serialized Variables ---------
+        [SerializeField] private Button negativeBtn;
+        [SerializeField] private Button positiveBtn;
+        [SerializeField] private Button yesButton;
+        [SerializeField] private Button noButton;
+        [SerializeField] private TextMeshProUGUI countText;
+        [SerializeField] private TextMeshProUGUI itemSellText;
+        #endregion ------------------
 
-        // #region --------- Private Variables ---------
-        // private BuyBoxController buyBoxController;
-        // private int itemCount;
-        // private int totalCost;
-        // #endregion ------------------
+        #region --------- Private Variables ---------
+        private SellBoxController buyBoxController;
+        private int itemCount;
+        private int totalCost;
+        #endregion ------------------
 
-        // #region --------- Public Variables ---------
-        // #endregion ------------------
+        #region --------- Public Variables ---------
+        #endregion ------------------
 
-        // #region --------- Monobehavior Methods ---------
-        // private void Awake()
-        // {
-        //     negativeBtn.onClick.AddListener(OnNegativeBtnClick);
-        //     positiveBtn.onClick.AddListener(OnPositiveBtnClick);
-        //     yesButton.onClick.AddListener(OnYesClick);
-        //     noButton.onClick.AddListener(OnNoClick);
-        // }
-        // #endregion ------------------
+        #region --------- Monobehavior Methods ---------
+        private void Awake()
+        {
+            negativeBtn.onClick.AddListener(OnNegativeBtnClick);
+            positiveBtn.onClick.AddListener(OnPositiveBtnClick);
+            yesButton.onClick.AddListener(OnYesClick);
+            noButton.onClick.AddListener(OnNoClick);
+        }
+        #endregion ------------------
 
-        // #region --------- Private Methods ---------
+        #region --------- Private Methods ---------
 
-        // #endregion ------------------
+        #endregion ------------------
 
-        // #region --------- Public Methods ---------
-        // public void SetBuyBoxView(int itemCount)
-        // {
-        //     gameObject.SetActive(false);
-        //     countText.text = itemCount.ToString();
-        //     UpdateBuyCounter(itemCount, itemCount);
-        //     DisableNegativeBtn();
-        // }
-        // public void SetBuyBoxController(BuyBoxController buyBoxController) => this.buyBoxController = buyBoxController;
+        #region --------- Public Methods ---------
+        public void SetSellBoxView(int itemCount)
+        {
+            gameObject.SetActive(false);
+            countText.text = itemCount.ToString();
+            UpdateSellCounter(itemCount, itemCount);
+            DisableNegativeBtn();
+        }
+        public void SetSellBoxController(SellBoxController buyBoxController) => this.buyBoxController = buyBoxController;
 
-        // public void OnNegativeBtnClick() => buyBoxController.DecrementItemCount();
+        public void OnNegativeBtnClick() => buyBoxController.DecrementItemCount();
 
-        // public void OnPositiveBtnClick() => buyBoxController.IncrementItemCount();
+        public void OnPositiveBtnClick() => buyBoxController.IncrementItemCount();
 
-        // public void UpdateBuyCounter(int itemCount, int itemBuyCost)
-        // {
-        //     this.itemCount = itemCount;
-        //     totalCost = itemCount * itemBuyCost;
-        //     countText.text = itemCount.ToString();
-        //     itemBuyText.text = totalCost.ToString();
-        // }
+        public void UpdateSellCounter(int itemCount, int itemSellCost)
+        {
+            this.itemCount = itemCount;
+            totalCost = itemCount * itemSellCost;
+            countText.text = itemCount.ToString();
+            itemSellText.text = totalCost.ToString();
+        }
 
-        // public void OnYesClick()
-        // {
-        //     if(GameManager.Instance.ValidateBuyTransaction(itemCount, totalCost))
-        //         buyBoxController.ResetItemCounter(true);
-        // }
+        public void OnYesClick()
+        {
+            if(GameManager.Instance.ValidateSellTransaction(itemCount, totalCost))
+                buyBoxController.ResetItemCounter(true);
+        }
 
-        // public void OnNoClick()
-        // {
-        //     gameObject.SetActive(false);
-        //     buyBoxController.ResetItemCounter(false);
-        // }
+        public void OnNoClick()
+        {
+            gameObject.SetActive(false);
+            buyBoxController.ResetItemCounter(false);
+        }
 
-        // public void EnableNegativeBtn() => negativeBtn.interactable = true;
+        public void EnableNegativeBtn() => negativeBtn.interactable = true;
 
-        // public void EnablePositiveBtn() => positiveBtn.interactable = true;
+        public void EnablePositiveBtn() => positiveBtn.interactable = true;
 
-        // public void DisablePositiveBtn() => positiveBtn.interactable = false;
+        public void DisablePositiveBtn() => positiveBtn.interactable = false;
 
-        // public void DisableNegativeBtn() => negativeBtn.interactable = false;
+        public void DisableNegativeBtn() => negativeBtn.interactable = false;
 
-        // public void EnableBuyBox() => gameObject.SetActive(true);
+        public void EnableSellBox() => gameObject.SetActive(true);
 
-        // #endregion ------------------
-    // }
+        #endregion ------------------
+    }
 }
