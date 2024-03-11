bbaa937a0388751f28766a066e931be0ca0ec5e0
Roopesh Verma <roopeshv.30@gmail.com>
2024-03-11 12:19:54 +0530

    remove item added event

--- Assets/Scripts/Events/EventService.cs
+++ Assets/Scripts/Events/EventService.cs
@@ -6,14 +6,12 @@ namespace InventoryShop.Services.Events
     {
         #region --------- Public Variables ---------
         public EventController<string, Sprite, string, int, int> OnItemClick;
-        public EventController<bool> onItemAdded;
         #endregion ------------------
 
         #region --------- Public Methods ---------
         public EventService()
         {
             OnItemClick = new EventController<string, Sprite, string, int, int>();
-            onItemAdded = new EventController<bool>();
         }
         #endregion ------------------
 
