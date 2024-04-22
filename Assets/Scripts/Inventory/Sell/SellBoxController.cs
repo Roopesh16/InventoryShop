005534824f435f5e5c5e3672a6126052fa97ffd3
Roopesh Verma <roopeshv.30@gmail.com>
2024-04-22 21:48:49 +0530

    Update Sell and Buy box mvc

--- Assets/Scripts/Inventory/Sell/SellBoxController.cs
+++ Assets/Scripts/Inventory/Sell/SellBoxController.cs
@@ -1,91 +1,44 @@
 using InventoryShop.Services;
-using InventoryShop.Services.Events;
-using InventoryShop.Managers;
+using InventoryShop.Transaction;
+
 namespace InventoryShop.Inventory.SellBox
 {
-    public class SellBoxController
+    public class SellBoxController : TransactionBoxController
     {
         #region --------- Private Variables ---------
 
         private InventoryService inventoryService;
-        private ItemService itemService;
-
-        private SellBoxModel sellBoxModel;
-        private SellBoxView sellBoxView;
-
-        private const int itemCount = 0;
         #endregion ------------------
 
         #region --------- Public Methods ---------
-        
-        public SellBoxController(SellBoxView sellBoxView, InventoryService inventoryService, ItemService itemService)
+
+        public SellBoxController(TransactionBoxView transactionBoxView, InventoryService inventoryService, ItemService itemService)
+            : base(transactionBoxView, itemService)
         {
             this.inventoryService = inventoryService;
-            this.itemService = itemService;
 
-            sellBoxModel = new(itemCount);
-            sellBoxModel.SetSellBoxController(this);
-
-            this.sellBoxView = sellBoxView;
-            this.sellBoxView.SetSellBoxView(itemCount);
-            this.sellBoxView.SetSellBoxController(this);
+            transactionBoxModel.SetController(this);
+            transactionBoxView.SetController(this);
         }
 
-        public void IncrementItemCount()
-        {
-            sellBoxModel.itemCount++;
-            sellBoxView.ToggleNegativeBtn(true);
-            if (sellBoxModel.itemCount >= sellBoxModel.itemQuantity)
-            {
-                sellBoxModel.itemCount = sellBoxModel.itemQuantity;
-                sellBoxView.TogglePositiveBtn(false);
-            }
-            sellBoxView.UpdateSellCounter(sellBoxModel.itemCount, sellBoxModel.itemSellCost);
-        }
-
-        public void DecrementItemCount()
-        {
-            sellBoxModel.itemCount--;
-            sellBoxView.TogglePositiveBtn(true);
-            if (sellBoxModel.itemCount <= 0)
-            {
-                sellBoxModel.itemCount = 0;
-                sellBoxView.ToggleNegativeBtn(false);
-            }
-            sellBoxView.UpdateSellCounter(sellBoxModel.itemCount, sellBoxModel.itemSellCost);
-        }
-
-        public void SetSellItemData(string itemName, int itemSellCost, int itemQuantity, float itemWeight)
-        {
-            sellBoxModel.SetItemData(itemName, itemSellCost, itemQuantity, itemWeight);
-            sellBoxView.EnableSellBox();
-        }
-
-        public void ResetItemCounter(bool hasSold)
+        public override void ResetItemCounter(bool hasSold)
         {
             if (hasSold)
             {
-                sellBoxModel.itemQuantity -= sellBoxModel.itemCount;
-                if (sellBoxModel.itemQuantity <= 0)
+                transactionBoxModel.itemQuantity -= transactionBoxModel.itemCount;
+                if (transactionBoxModel.itemQuantity <= 0)
                 {
-                    sellBoxModel.itemQuantity = 0;
+                    transactionBoxModel.itemQuantity = 0;
                     inventoryService.DisableDescription();
-                    sellBoxView.TogglePositiveBtn(false);
-                    sellBoxView.ToggleNegativeBtn(false);
-                    itemService.RemoveInventoryItem(sellBoxModel.itemName);
+                    transactionBoxView.TogglePositiveBtn(false);
+                    transactionBoxView.ToggleNegativeBtn(false);
+                    itemService.RemoveInventoryItem(transactionBoxModel.itemName);
                 }
-                inventoryService.SetItemQuantity(sellBoxModel.itemQuantity);
-                itemService.AddShopItems(sellBoxModel.itemName, sellBoxModel.itemCount);
+                inventoryService.SetItemQuantity(transactionBoxModel.itemQuantity);
+                itemService.AddShopItems(transactionBoxModel.itemName, transactionBoxModel.itemCount);
             }
 
-            sellBoxModel.itemCount = 0;
-            sellBoxView.UpdateSellCounter(sellBoxModel.itemCount, sellBoxModel.itemSellCost);
-        }
-
-        public void ValidateSellTransaction()
-        {
-            if (GameManager.Instance.ValidateSellTransaction(sellBoxModel.itemCount, sellBoxModel.itemCount * sellBoxModel.itemSellCost, sellBoxModel.itemCount * sellBoxModel.itemWeight))
-                ResetItemCounter(true);
+           base.ResetItemCounter(hasSold);
         }
         #endregion ------------------
     }
--- Assets/Scripts/Shop/Buy/BuyBoxController.cs
+++ Assets/Scripts/Shop/Buy/BuyBoxController.cs
@@ -1,92 +1,45 @@
 using InventoryShop.Services;
-using InventoryShop.Managers;
+using InventoryShop.Transaction;
 
 namespace InventoryShop.Shop.BuyBox
 {
-    public class BuyBoxController
+    public class BuyBoxController : TransactionBoxController
     {
         #region --------- Private Variables ---------
 
         private ShopService shopService;
-        private ItemService itemService;
-
-        private BuyBoxModel buyBoxModel;
-        private BuyBoxView buyBoxView;
-
-        private const int itemCount = 0;
         #endregion ------------------
 
         #region --------- Public Methods ---------
-        
-        public BuyBoxController(BuyBoxView buyBoxView, ShopService shopService, ItemService itemService)
+
+        public BuyBoxController(TransactionBoxView transactionBoxView, ShopService shopService, ItemService itemService) 
+            : base(transactionBoxView, itemService)
         {
             this.shopService = shopService;
             this.itemService = itemService;
 
-            buyBoxModel = new(itemCount);
-            buyBoxModel.SetBuyBoxController(this);
-
-            this.buyBoxView = buyBoxView;
-            this.buyBoxView.SetBuyBoxView(itemCount);
-            this.buyBoxView.SetBuyBoxController(this);
+            transactionBoxModel.SetController(this);
+            transactionBoxView.SetController(this);
         }
 
-        public void IncrementItemCount()
-        {
-            buyBoxModel.itemCount++;
-            buyBoxView.ToggleNegativeBtn(true);
-            if (buyBoxModel.itemCount >= buyBoxModel.itemQuantity)
-            {
-                buyBoxModel.itemCount = buyBoxModel.itemQuantity;
-                buyBoxView.TogglePositiveBtn(false);
-            }
-            buyBoxView.UpdateBuyCounter(buyBoxModel.itemCount, buyBoxModel.itemBuyCost);
-        }
 
-        public void DecrementItemCount()
-        {
-            buyBoxModel.itemCount--;
-            buyBoxView.TogglePositiveBtn(true);
-            if (buyBoxModel.itemCount <= 0)
-            {
-                buyBoxModel.itemCount = 0;
-                buyBoxView.ToggleNegativeBtn(false);
-            }
-            buyBoxView.UpdateBuyCounter(buyBoxModel.itemCount, buyBoxModel.itemBuyCost);
-        }
-
-        public void SetBuyItemData(string itemName, int itemBuyCost, int itemQuantity, float itemWeight)
-        {
-            buyBoxModel.SetItemData(itemName, itemBuyCost, itemQuantity, itemWeight);
-            buyBoxView.EnableBuyBox();
-        }
-
-        public void ResetItemCounter(bool hasBought)
+        public override void ResetItemCounter(bool hasBought)
         {
             if (hasBought)
             {
-                buyBoxModel.itemQuantity -= buyBoxModel.itemCount;
-                if (buyBoxModel.itemQuantity <= 0)
+                transactionBoxModel.itemQuantity -= transactionBoxModel.itemCount;
+                if (transactionBoxModel.itemQuantity <= 0)
                 {
-                    buyBoxModel.itemQuantity = 0;
+                    transactionBoxModel.itemQuantity = 0;
                     shopService.DisableDescription();
-                    buyBoxView.TogglePositiveBtn(false);
-                    buyBoxView.ToggleNegativeBtn(false);
+                    transactionBoxView.TogglePositiveBtn(false);
+                    transactionBoxView.ToggleNegativeBtn(false);
                 }
-                shopService.SetItemQuantity(buyBoxModel.itemQuantity);
-                itemService.AddInventoryItems(buyBoxModel.itemName, buyBoxModel.itemCount);
+                shopService.SetItemQuantity(transactionBoxModel.itemQuantity);
+                itemService.AddInventoryItems(transactionBoxModel.itemName, transactionBoxModel.itemCount);
             }
 
-            buyBoxModel.itemCount = 0;
-            buyBoxView.UpdateBuyCounter(buyBoxModel.itemCount, buyBoxModel.itemBuyCost);
-        }
-
-        public void ValidateBuyTransaction()
-        {
-            if (GameManager.Instance.ValidateBuyTransaction(buyBoxModel.itemCount,
-                                                            buyBoxModel.itemCount*buyBoxModel.itemBuyCost,
-                                                            buyBoxModel.itemCount*buyBoxModel.itemWeight))
-                ResetItemCounter(true);
+            base.ResetItemCounter(hasBought);
         }
         #endregion ------------------
     }
